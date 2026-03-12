bcc0f3b3305e16d7ef3b0437547c329a4c279679
Natalia Leon <natalia.leon4@upr.edu>
2026-03-12 19:42:53 -0400

    Revert "Merge branch 'main' into Score-Features"
    This reverts commit 3e8e94dbc77b3a1fcfe0f4003a39f7ee14072768, reversing
    changes made to cea1f9e0591069c6632201005f4ac4a8d9132329.

--- src/Program.cpp
+++ src/Program.cpp
@@ -17,8 +17,8 @@ Program::Program() {
         });
 
     for (int i = 0; i < 30; i++) {
-        float x = 250 + 50 * (i % 10);
-        float y = 200 + 50 * (i / 10);
+        float x = 250 + 50 * i;
+        float y = 200 + 50 * i;
 
         Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
             std::pair<float, float>{x, y}, 
@@ -67,11 +67,7 @@ void Program::Update() {
 
         for (Projectile& p : Projectile::projectiles) { 
             p.update(); 
-            
-            if (p.ID == 1 && HitBox::Collision(p.getHitBox(), player->hitBox)){
-                p.del = true;
-                PlayerReset();
-            }
+
         }
 
         if (lives <= 0 && pauseFrames <= 0) gameOver = true;
@@ -211,24 +207,4 @@ void Program::Reset() {
     count = 0;
     delay = 0;
     lives = 3;
-
-    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
-        std::pair<float, float>{350, 150},
-        new SpEnemy(350, 150)
-    });
-
-    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
-        std::pair<float, float>{600, 150},
-        new SpEnemy(600, 150)
-    });
-
-    for (int i = 0; i < 30; i++) {
-        float x = 250 + 50 * (i % 10);
-        float y = 200 + 50 * (i / 10);
-
-        Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
-            std::pair<float, float>{x, y},
-            new StdEnemy(x, y)
-        });
-    }
 }
